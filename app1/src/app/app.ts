a3e0815aea502966c57b331f781adfc66a23e16c
LucasAntonio-LAM <lucasantonions45@gmail.com>
2026-01-20 20:07:59 -0300

    components

--- app1/src/app/app.ts
+++ app1/src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+
+import { TopoComponent } from './topo/topo';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [TopoComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
